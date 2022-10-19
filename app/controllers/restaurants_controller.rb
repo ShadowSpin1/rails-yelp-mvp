0c0bb9d5932d4c578574e927e1f0632ab510dd2a
Ricky Tran <diabloxtran@gmail.com>
2022-10-18 21:05:38 -0400

    created functional views that now need to be connected

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,28 @@
+class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only: %i[create show]
+
+  def create
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show; end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurants).permit(:name, :address, :phone_number, :category)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+end
