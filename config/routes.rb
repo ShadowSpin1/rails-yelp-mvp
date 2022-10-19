657c3c5690afd7b132db1bd7c15f5b0b81fb2bdc
Ricky Tran <diabloxtran@gmail.com>
2022-10-18 20:32:31 -0400

    created all routes and seed restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    resources :restaurants, only: %i[create index new show] do
+      resources :reviews, only: %i[create new]
+    end
+  end
 end
